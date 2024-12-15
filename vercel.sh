f027f32dfa27ce11f509b7926eb38297c01d9efc
Travcort <tarvkoome@gmail.com>
2024-12-15 21:37:49 +0300

    Build File

---
+++ vercel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Exit on any failure
+set -e
+
+# Installing Project Dependecies for the Backend
+echo "Installing Project Dependencies for the backend..."
+npm install
+
+
+if [ -d 'frontend' ]; then
+    # Installing Project Dependecies for the Frontend
+    echo "Installing Project Dependencies for the frontend..."
+    npm install --prefix frontend
+
+    # Building the Frontend
+    echo "Building the frontend..."
+    npm run build --prefix frontend
+else
+    echo "The Frontend Directory is missing!"
+fi
+
+# Starting the Server
+echo "Running the Server..."
+npm start
+
+echo "Build Completed!"
